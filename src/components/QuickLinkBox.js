5ae8b2b45cd7edb3f7931963f3d0598d44e7d143
Steffen Svenningsen <stef7625@gmail.com>
2023-05-20 10:51:49 +0200

    Temp box styling for link boxes

--- src/components/QuickLinkBox.js
+++ src/components/QuickLinkBox.js
@@ -5,7 +5,7 @@ export default function QuickLinkBox(props) {
   return (
     <NavLink className="quick-link-box" to={props.linkPath}>
         <div>
-            <img src={props.imgSrc} alt={props.imgDesc} />
+            <img src={props.imgSrc} width={480} height={320} alt={props.imgDesc} />
         </div>
         <h1>{props.title}</h1>
     </NavLink>
